1ccc7e2a991797c970b4b5bbfdc418cb04f44264
Akash <hafejmohammadakash@gmail.com>
2025-05-22 09:48:08 +0600

    01 server basic setup

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
+
+app.get("/", async (req, res) => {
+    res.send(`server is running on the PORT: ${port}`)
+});
+
+app.listen(port, () => {
+    console.log(`successfully mongoDB Connected.... ${port}`)
+})
