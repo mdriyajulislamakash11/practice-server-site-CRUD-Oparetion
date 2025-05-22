ce337eba37a290f3cc9a45bfda2eedda071dbde8
Akash <hafejmohammadakash@gmail.com>
2025-05-22 11:10:04 +0600

    user or post method

--- server/index.js
+++ server/index.js
@@ -1,16 +1,64 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// server-site
+// eeGtAr14AFyWPSnw
+
+const uri =
+  "mongodb+srv://server-site:eeGtAr14AFyWPSnw@cluster0.zchez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const userCollection = client.db("sample-users").collection("users");
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // post 
+    app.post("/users", async (req, res) =>{
+        const user = req.body;
+        console.log(user)
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", async (req, res) => {
-    res.send(`server is running on the PORT: ${port}`)
+  res.send(`server is running on the PORT: ${port}`);
 });
 
 app.listen(port, () => {
-    console.log(`successfully mongoDB Connected.... ${port}`)
+  console.log(`successfully mongoDB Connected.... ${port}`);
 });
