10bbe0525c907ba2dbb5d83f899d33c4aadc8329
Akash <hafejmohammadakash@gmail.com>
2025-05-22 11:20:17 +0600

    03 delete

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -41,7 +41,15 @@ async function run() {
         console.log(user)
         const result = await userCollection.insertOne(user);
         res.send(result);
-    })
+    });
+
+    // delete 
+    app.delete("/users/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await userCollection.deleteOne(query)
+        res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
